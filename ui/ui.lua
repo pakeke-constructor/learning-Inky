3ceaf20fc18882762f05a1bda5f8d648fc448ae1
Oli <OliverWDGarrett@gmail.com>
2023-10-14 00:05:06 +1300

    ratio objects support max/min hright

--- ui/ui.lua
+++ ui/ui.lua
@@ -6,46 +6,52 @@
 local ui = {}
 rawset(_G, "ui", ui)
 
+local lookupError = {
+    __index = function(t,k)
+        error("Unknown key: " .. tostring(k))
+    end
+}
 
 
-function ui.renderScreenRatio(ratioObj, X,Y,W,H)
-    local x,y,w,h
-	local W,H = love.graphics.getDimensions()
+setmetatable(ui, lookupError)
 
-    w = ratio.w * W
-    h = ratio.h * H
 
-	x = x*W - w/2
-	y = y*H - h/2
-	
-	element:render(x,y,w,h)
+ui.ratios = require("ui.ratios")
+setmetatable(ui.ratios, lookupError)
+
+
+local function assertRatio(ratio)
+    assert(ratio.x and ratio.y and ratio.w and ratio.h, "invalid ratio obj")
 end
 
 
-function ui.getRatio(ratio, X,Y,W,H)
+local function clamp(v, lo, hi)
+	return math.max(lo, math.min(v, hi))
+end
+
+
+function ui.getView(ratio, X,Y,W,H)
+    --[[
+        takes a ratio object, and returns appropriate x,y,w,h 
+
+        ratioObj: {
+            x 
+        }
+    ]]
     local x,y,w,h
+    assertRatio(ratio)
 
     w = ratio.w * W
     h = ratio.h * H
 
 	x = X + x*W - w/2
 	y = Y + y*H - h/2
+    
+    w = clamp(w, ratio.minWidth or w, ratio.maxWidth or w)
+    h = clamp(h, ratio.minHeight or h, ratio.maxHeight or h)
 	return x,y,w,h
 end
 
-
-function ui.renderRatio(parent, child, ratio)
-    local x,y,w,h
-    local X,Y,W,H = parent:getView()
-
-    w = ratio.w * W
-    h = ratio.h * H
-
-	x = x*W - w/2
-	y = y*H - h/2
-	
-	child:render(x,y,w,h)
-end
 
 
 ui.elems = {}
