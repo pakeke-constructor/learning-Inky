ff85a293aa4e284d23bfbb7b587e803af03b8058
Oli <OliverWDGarrett@gmail.com>
2023-10-15 13:10:54 +1300

    created Inky wrapper

---
+++ COMPONENTS_PLANNING.md
@@ -0,0 +1,26 @@
+
+
+# What components do we want?
+
+**Text component**
+- auto text wrap
+- shrinks text to fit in region
+- .text prop
+
+
+**Text Input component**
+- See `keyboardPointer` idea in discord
+
+**Number slider component**
+- minValue/maxValue
+
+**Button component**
+- .onClick prop
+- plays a button when pressed
+
+
+**Scrollbar component**
+DOESN'T INCLUDE SCROLLING! Just the scrollbar
+
+
+
--- ui/ui.lua
+++ ui/ui.lua
@@ -16,14 +16,52 @@ local lookupError = {
 setmetatable(ui, lookupError)
 
 
+local elements = {--[[
+    [elemName] -> ElementFactory
+]]}
+ui.elements = elements
 
 
 
-ui.elems = {}
+function ui.defineElement(elemName, setupFunc)
+    --[[
+    USAGE:
+
+    return ui.defineElement("ElementName", function(self, args)
+        for k,v in pairs(args) do
+            print("arg passed in: ", k, v)
+        end
+
+        return function(region)
+            -- `region` is a kirigami region
+            ...
+        end
+    end)
+    ]]
+    assert(not elements[elemName], "overwriting element")
+
+    local Element = Inky.defineElement(function(elem)
+        return function(_, x,y,w,h)
+            local region = kirigami.Region(x,y,w,h)
+            elem.props.renderFunction(region)
+        end
+    end)
+
+    local function ctor(scene, ...)
+        local elem = Element(scene)
+        elem.props.renderFunction = setupFunc(elem, ...)
+        assert(type(elem.props.renderFunction) == "function", "need to return function")
+        return elem
+    end
+
+    elements[elemName] = ctor
+    return ctor
+end
+
+
 
 local loadTree = require("load_tree")
-
-loadTree("ui/elems", ui.elems)
+loadTree("ui/elems")
 
 
 
